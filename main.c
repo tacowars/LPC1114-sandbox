605dd194878d3075f80b9be4ca6207211fb87f82
Patrick Sanchez <patrick@tacowars.com>
2012-09-03 19:49:52 -0500

    MIDI clock experiment

--- main.c
+++ main.c
@@ -28,6 +28,9 @@ volatile unsigned long IRQTickCnt=0;      /* fast counter                    */
 extern volatile uint32_t UARTCount;
 extern volatile uint8_t UARTBuffer[BUFSIZE];
 
+// TImer OSC
+volatile unsigned int Frequency = 65000; // lower is higher
+
 void SysTick_Handler (void) {           /* SysTick Interrupt Handler (~1ms)    */
 	SysTickCnt++;
 	if (IRQTickCnt >= 100) {
@@ -35,6 +38,8 @@ void SysTick_Handler (void) {           /* SysTick Interrupt Handler (~1ms)    *
 		IRQTickCnt = 0;
 	}
 	IRQTickCnt++;
+
+
 }
 
 void Delay (unsigned long tick) {       /* Delay Function                     */
@@ -59,32 +64,52 @@ void LEDinit(void) {
 }
 
 void TMR16init() {
-	unsigned int Frequency = 65000; // lower is higher
 	// System AHB clock control register - Enable Timer16B0 Table 21, P30 in user manual
 	LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 7);
-
 	/* IOCON_PIO0_8 register - Enable CT16B0_MAT0 pin p85*/
 	LPC_IOCON->PIO0_8 |= (1<<1); // enabling bit 1 sets match pin
+	/* IOCON_PIO0_8 register - Enable CT16B0_MAT0 pin p85*/
+	LPC_IOCON->PIO0_9 |= (1<<1); // enabling bit 1 sets match pin
+	/* IOCON_PIO0_8 register - Enable CT16B0_MAT1 pin p85*/
+	LPC_IOCON->SWCLK_PIO0_10 |= (6>>1); // enabling bit 1 sets match pin
 
-	/* Enable PWM function on CT32B0_MAT0 pin */
-	LPC_TMR16B0->PWMC |= (1<<0); // 65535 16bit TOP 1800hz?
+	/* Enable PWM function on all CT16B0_MATx pins */
+	LPC_TMR16B0->PWMC |= (2<<1); // 65535 16bit TOP 1800hz?
 
 	/* Set the PWM frequency */
 	LPC_TMR16B0->MR3 = Frequency; // p.327 in UM
-
 	/* Set initial duty cycle */
 	LPC_TMR16B0->MR0 = Frequency / 2; // p.327 in UM
+	LPC_TMR16B0->MR1 = Frequency / 2; // p.327 in UM
 
+	LPC_TMR16B0->MR2 = Frequency / 2; // p.327 in UM
 	/* TMR16B0MCR - Reset on MR3 p.330 in UM */
 	LPC_TMR16B0->MCR |= (1<<10); // Reset on MR3: the TC will be reset if MR3 matches it.
-
 	LPC_TMR16B0->PR = 5; // The 16-bit Prescale Register specifies the maximum value for the Prescale Counter.
 	LPC_TMR16B0->PC = 2; // Prescale Counter register
-
 	/* Count Control Register (TMR16B0TCR - Start Timer16B0 p.329 in UM */
 	LPC_TMR16B0->TCR = 1;// |= (1<<1);
-
 }
+
+void playNote(uint8_t note) {
+	Frequency = 65000 - (note * 2500);
+
+	/* Set the PWM frequency */
+	LPC_TMR16B0->MR3 = Frequency; // p.327 in UM
+	/* Set initial duty cycle */
+	LPC_TMR16B0->MR0 = Frequency / 2; // p.327 in UM
+}
+
+void beatLight() {
+	LPC_TMR16B0->MR0 = 65000; // (65000 / 1000) = 65
+	LPC_TMR16B0->MR1 = 65000;
+}
+
+void beatLightOff() {
+	LPC_TMR16B0->MR0 = 0; // (65000 / 1000) = 65
+	LPC_TMR16B0->MR1 = 0;
+}
+
 
 int main(void) {
 
@@ -94,22 +119,46 @@ int main(void) {
 	TMR16init();
 	UARTInit(UART_BAUD);
 
-	uint8_t bootmsg[] = "-- lil' ARM booted:\n";
-	UARTSend((uint8_t *)bootmsg,21);
-
 	while(1) {
 		//Delay(1000);
 
-		if (UARTCount > 0) {
-			LPC_UART->IER = IER_THRE | IER_RLS;			/* Disable UART Interrupt Enable Register */
-			int i = 0;
-			for (i=0;UARTCount > 0;i++) {
-				UARTSend(UARTBuffer[i],1);
-				//UARTSend( (uint8_t *)UARTBuffer, UARTCount );
+		if (UARTCount > 0) { // MIDI commands are 3 bytes
+			while (UARTCount > 0) {
+				LPC_UART->IER = IER_THRE | IER_RLS;			/* Disable UART Interrupt Enable Register */
+				switch ( *UARTBuffer ) {
+				case 0x90: // NOTE ON, CH0
+					if (UARTCount >= 3) { // wait for entire command
+						*UARTBuffer = *(UARTBuffer+1); // skip the first byte
+						UARTSend( (uint8_t *)UARTBuffer, 1 ); // send only note pitch
+						playNote(*UARTBuffer);
+						*UARTBuffer = *(UARTBuffer+1);
+						UARTCount-=3;
+					}
+				  break;
+				case 0xfa:
+					LPC_GPIO0->DATA ^= (1<<3); /* toggle GPIOX_X */
+					UARTSend( (uint8_t *)UARTBuffer, 1 );
+					beatLight();
+					UARTCount--;
+				  break;
+				case 0xf8:
+					LPC_GPIO0->DATA ^= (1<<3); /* toggle GPIOX_X */
+					UARTSend( (uint8_t *)UARTBuffer, 1 );
+					UARTCount--;
+				  break;
+				case 0xfc:
+					LPC_GPIO0->DATA &= ~(1<<3); /* clear GPIOX_X */
+					beatLightOff();
+					UARTSend( (uint8_t *)UARTBuffer, 1 );
+					UARTCount--;
+				  break;
+				default:
+					UARTSend( (uint8_t *)UARTBuffer, 1 );
+					UARTCount--;
+				  break;
+				}
+				LPC_UART->IER = IER_THRE | IER_RLS | IER_RBR;	/* Re-enable UART Interrupt Enable Register */
 			}
-			UARTSend( (uint8_t *)UARTBuffer, UARTCount );
-			UARTCount = 0;
-			LPC_UART->IER = IER_THRE | IER_RLS | IER_RBR;	/* Re-enable UART Interrupt Enable Register */
 		}
 	}
 	return 0 ;
